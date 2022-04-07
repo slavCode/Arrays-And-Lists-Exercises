606c093c2e341b9bb8d3f015d2aed8081990d717
Slav Tomov <stomow@outlook.com>
2022-04-07 21:02:49 +0300

    Add Index Of Letters

---
+++ IndexOfLetters/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace IndexOfLetters
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string alphabet = "abcdefghijklmnopqrstuvwxyz";
+            var alphabetArr = new char[alphabet.Length];
+            for (int i = 0; i < alphabet.Length; i++) alphabetArr[i] = alphabet[i];
+
+            var input = Console.ReadLine();
+            var inputChar = new char[input.Length];
+            for (int i = 0; i < input.Length; i++) inputChar[i] = input[i];
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                for (int j = 0; j < alphabetArr.Length; j++)
+                {
+                    if (inputChar[i] == alphabetArr[j]) Console.WriteLine($"{inputChar[i]} -> {j}");
+                }
+            }
+        }
+    }
+}
---
+++ MostFrequentNumber/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace MostFrequentNumber
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int[] numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            int bestCounter = 0;
+            int mostFreqNum = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int currCounter = 0;
+
+                for (int j = i + 1; j < numbers.Length - 1; j++)
+                {
+                    if (numbers[i] == numbers[j]) currCounter++;
+                }
+
+                if (currCounter > bestCounter)
+                {
+                    bestCounter = currCounter;
+                    mostFreqNum = numbers[i];
+                }
+            }
+            Console.WriteLine(mostFreqNum);
+        }
+    }
+}
