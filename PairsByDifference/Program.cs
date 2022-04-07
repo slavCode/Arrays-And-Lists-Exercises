cd2302e215556e4aeec8e0721b3af8d258000966
Slav Tomov <stomow@outlook.com>
2022-04-07 21:24:15 +0300

    Pairs by Difference

---
+++ PairsByDifference/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace PairsByDifference
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            var numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int diff = int.Parse(Console.ReadLine());
+
+            int counter = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int numToCompare = numbers[i];
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    if (Math.Abs(numToCompare - numbers[j]) == diff) counter++;
+                }
+            }
+
+            Console.WriteLine(counter);
+        }
+    }
+}
